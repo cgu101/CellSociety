40085f2aa048086da603c9cf489f679577a80bb1
Christopher Streiffer <cds33@duke.edu>
2015-09-15 14:23:30 -0400

    Start screen is working. Dynamically loads cell simulations specified in config file. Dynamically loads the cell files corresponding xml files.

--- src/cellsociety/screen/StartScreen.java
+++ src/cellsociety/screen/StartScreen.java
@@ -3,24 +3,23 @@ package cellsociety.screen;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.sun.prism.paint.Color;
-
 import cellsociety.managers.ConfigManager;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 
 public class StartScreen extends AbstractScreen {
 	private int WIDTH;
 	private int HEIGHT;
 	private List<Icon> iconList;
 	private Text titleBox;
+	private String titleName = "";
+	private ComboBox<String> xmlLoader;
 
 	@Override
 	protected void init() {
@@ -30,29 +29,49 @@ public class StartScreen extends AbstractScreen {
 		scene = new Scene(root, WIDTH, HEIGHT);
 		initIconList();
 		initTitle();
+		initXmlList();
 	}
 	
 	private void initIconList() {
 		iconList = new ArrayList<Icon>();
 		List<String> iconNames = ConfigManager.getStringList(ConfigManager.scope(this.getClass().getName(), "iconNames"));
 		
+		int offset = WIDTH/iconNames.size()/2;
 		for(String s: iconNames) {
-			Icon toAdd = new Icon(s);
+			Icon toAdd = new Icon(s, offset);
 			iconList.add(toAdd);
 			root.getChildren().add(toAdd);
+			offset=offset+WIDTH/iconNames.size();
 		}
 	}
 	
 	private void initTitle() {
-		Font f = Font.loadFont(getClass().getClassLoader().getResourceAsStream("SECRCODE.TTF"), 40);
-		Text t = new Text(400, 50, "Cell Society");
-		t.setFont(f);
-		
-		titleBox = new Text(400, 100, "");
-		titleBox.setFont(f);
-		
+		createText("Cell Society", HEIGHT/6, 40);
+		titleBox = createText("", HEIGHT/6+40, 20);
+	}
+	
+	private void initXmlList() {
+		xmlLoader = new ComboBox<String>();
+		xmlLoader.setPromptText("XML File to Load");
+		xmlLoader.setEditable(true);   
+		xmlLoader.setLayoutX(ConfigManager.getInt(ConfigManager.scope(this.getClass().getName(), "xml-x")));
+		xmlLoader.setLayoutY(ConfigManager.getInt(ConfigManager.scope(this.getClass().getName(), "xml-y")));
+		root.getChildren().add(xmlLoader);
+	}
+	
+	private Text createText(String s, int yPos, int size) {
+		Font font = Font.loadFont(getClass().getClassLoader().getResourceAsStream("SECRCODE.TTF"), size);
+		Text t = new Text(s);
+		t.setFont(font);
+		t.setLayoutX((WIDTH-t.getBoundsInLocal().getWidth())/2);
+		t.setLayoutY(yPos);
 		root.getChildren().add(t);
-		root.getChildren().add(titleBox);
+		return t;
+	}
+	
+	private void formatText(Text t, String s) {
+		t.setText(s);
+		t.setLayoutX((WIDTH-t.getBoundsInLocal().getWidth())/2);
 	}
 	
 	@Override
@@ -60,37 +79,39 @@ public class StartScreen extends AbstractScreen {
 		for(Icon i: iconList) {
 			i.setOnAction(e->i.handleAction());
 			i.setOnMouseEntered(e->i.handleHover());
+			i.setOnMouseExited(e->i.handleExit());
 		}
 	}
-	
 	
 	private class Icon extends Button {
 		private ImageView image;
 		private String iconName;
+		private List<String> xmlList;
 		
-		private Icon(String s) {
-			iconName = s;
+		private Icon(String s, int xOffset) {
+			xmlList = ConfigManager.getStringList(ConfigManager.scope(s, "xmlList"));
 			Image img = new Image(getClass().getClassLoader().getResourceAsStream(ConfigManager.getString(ConfigManager.scope(s, "image"))));
 			image = new ImageView(img);
 			setGraphic(image);	
+			setLayoutY(ConfigManager.getInt(ConfigManager.scope(this.getClass().getName(), "yPos"), HEIGHT/3));
+			setLayoutX(xOffset-(image.getBoundsInParent().getWidth()+18)/2);
+			iconName = s;
 		}
 		
 		private void handleAction() {
-			System.out.println("Clicking: " + iconName);
+			titleName = iconName;
+			if(xmlLoader.getItems() != null) {
+				xmlLoader.getItems().clear();
+			}
+			xmlLoader.getItems().addAll(xmlList);
 		}
 		
 		private void handleHover() {
-			System.out.println("Hovering: " + iconName);
-			titleBox.setText(iconName);
+			formatText(titleBox, iconName);
+		}
+		
+		private void handleExit() {
+			formatText(titleBox, titleName);
 		}
 	}
-	
-	// StartScreen testing
-//	public static void main(String...args) {
-//		StartScreen test = new StartScreen();
-//		System.out.println(String.format(test.getClass().getName(), "width"));
-//		System.out.println(test.WIDTH);
-//		System.out.println(test.HEIGHT);
-//
-//	}
 }
