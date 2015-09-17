c816fa15c8e40be007d352dbb6293a1a3344fdbe
Connor Usry <cgu1001@gmail.com>
2015-09-17 12:58:35 -0400

    Working on FireGrid Implementation of status changing

--- src/cellsociety/grid/FireGrid.java
+++ src/cellsociety/grid/FireGrid.java
@@ -1,5 +1,6 @@
 package cellsociety.grid;
 
+import cellsociety.cell.AbstractCell;
 import cellsociety.managers.ConfigManager;
 import cellsociety.parameters.FireParameters;
 import javafx.scene.Group;
@@ -12,6 +13,34 @@ public class FireGrid extends AbstractGrid {
 		makeScene();
 		myParameters = new FireParameters();
 		this.scene = myParameters.getScene();
+	}
+	
+	@Override
+	public void run(){
+		//for each cell in map 
+			//check if cell is on fire
+				//(Set cell's next state to empty)
+		
+				//if cell is on fire... grab neighbors and put in set
+				//for each neighbor... if (probCatch)
+				//nextState = fire 
+		
+		AbstractCell cell = null;
+		for(int row = 0; row < map.length; row++){
+			for(int col = 0; col < map[row].length; col++){
+				cell = map[row][col];
+				if(cell.toString() == "onFire"){
+					spreadFire(row,col);
+				}
+			}
+		}
+		
+	}
+
+	private void spreadFire(int row, int col) {
+		AbstractCell cell = map[row][col];
+		//cell.changeState();
+		//No
 	}
 	
 }
