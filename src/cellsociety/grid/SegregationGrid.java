d6d7af89b9579f9479a809704d67c51a7f2a25f9
Christopher Streiffer <cds33@duke.edu>
2015-09-18 00:35:57 -0400

    Initial commit for new grid

--- src/cellsociety/grid/SegregationGrid.java
+++ src/cellsociety/grid/SegregationGrid.java
@@ -1,5 +1,25 @@
 package cellsociety.grid;
 
+import cellsociety.parameters.FireParameters;
+
 public class SegregationGrid extends AbstractGrid {
+	
+	private int similar;
+	private boolean satisfied;
+	
+	public SegregationGrid(String s) {
+		
+	}
+	
+	@Override
+	protected void init() {
+		makeScene();
+		myParameters = new SegregationParameters();
+	}
+	
+	@Override
+	public void run() {
+		
+	}
 	
 }
