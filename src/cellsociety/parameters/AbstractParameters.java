819c83d88d3eadfe8947a6add847552a1b849e70
David Zhou <xdavidzhou@gmail.com>
2015-09-17 20:16:38 -0400

    completed Parameter Toggles

--- src/cellsociety/parameters/AbstractParameters.java
+++ src/cellsociety/parameters/AbstractParameters.java
@@ -1,49 +1,117 @@
 package cellsociety.parameters;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import cellsociety.managers.ConfigManager;
+import cellsociety.parameters.AbstractParameters.Toggle;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 public abstract class AbstractParameters {
 
-	protected int WIDTH = 500;
+	protected int WIDTH = 250;
 	protected int HEIGHT = 500;
-	
-	
+
 	protected Scene paramScene;
 	protected Group root;
-	protected List<ParamSlider> sliderList;
-	
+	protected List<Toggle> sliderList;
+	protected HashMap<String, Double> values;
+	protected Font font = Font.loadFont(getClass().getClassLoader().getResourceAsStream("SECRCODE.TTF"),
+			ConfigManager.getInt(ConfigManager.scope(this.getClass().getName(), "FontSize")));
+
 	public AbstractParameters() {
 		this.root = new Group();
 		this.paramScene = new Scene(root, WIDTH, HEIGHT);
-		this.sliderList = new ArrayList<ParamSlider>();
+		this.sliderList = new ArrayList<Toggle>();
+		this.values = new HashMap<String, Double>();
 		init();
 	}
 
-	abstract protected void init();
-	
+	protected void init() {
+		List<String> sliderNames = ConfigManager.getStringList(ConfigManager.scope(this.getClass().getName(), "names"));
+		double yOffset = 0;
+		for (String curr : sliderNames) {
+			List<Double> sliderValues = ConfigManager
+					.getDoubleList(ConfigManager.scope(this.getClass().getName(), curr));
+			Toggle nextToggle = new Toggle(curr, sliderValues.get(0), sliderValues.get(1), sliderValues.get(2));
+			values.put(curr, sliderValues.get(0));
+			nextToggle.setLayoutX(ConfigManager.getInt(ConfigManager.scope(this.getClass().getName(), "x")));
+			yOffset += ConfigManager.getInt(ConfigManager.scope(this.getClass().getName(), "y"));
+			nextToggle.setLayoutY(yOffset);
+			sliderList.add(nextToggle);
+			root.getChildren().add(nextToggle);
+		}
+	}
+
 	public Scene getScene() {
 		return paramScene;
 	}
-	
-	protected class ParamSlider extends Slider {
+
+	public double getValue(String input) {
+		return values.get(input);
+	}
+
+	protected class Toggle extends VBox {
 		private String name;
-		
-		protected ParamSlider(String n, Double v, Double y, Double double1) {
+		private Label nameLabel;
+		private Slider slider;
+		private TextField valueField;
+
+		protected Toggle(String n, Double x, Double y, Double z) {
 			name = n;
-			setValue(v);
-			setMin(y);
-			setMax(double1);
+			slider = new Slider();
+			slider.setValue(x);
+			slider.setMin(y);
+			slider.setMax(z);
+			slider.setShowTickMarks(true);
+			slider.setShowTickLabels(true);
+			slider.setMajorTickUnit((slider.getMax() - slider.getMin()));
+			nameLabel = new Label(name + ":");
+			nameLabel.setFont(font);
+			valueField = new TextField(String.format("%.2f", slider.getValue()));
+			valueField.setPrefColumnCount(valueField.getText().length());
+			valueField.setAlignment(Pos.CENTER);
+			valueField.textProperty()
+					.addListener((ObservableValue<? extends String> ob, String oldVal, String newVal) -> {
+						valueField.setPrefColumnCount(valueField.getText().length());
+					});
+			valueField.setOnAction(e -> {
+				try {
+					String current = this.valueField.getCharacters().toString();
+					double val = Double.parseDouble(current);
+					if (val >= this.slider.getMin() && val <= this.slider.getMax()) {
+						this.slider.setValue(val);
+						values.put(name, val);
+					} else {
+						valueField.setText(String.format("%.2f", this.slider.getValue()));
+					}
+				} catch (NumberFormatException d) {
+					valueField.setText(String.format("%.2f", this.slider.getValue()));
+				}
+			});
+			HBox lowerBox = new HBox();
+			lowerBox.getChildren().addAll(slider, valueField);
+			this.getChildren().addAll(nameLabel, lowerBox);
+			slider.valueProperty().addListener((ObservableValue<? extends Number> ov, Number oldVal, Number newVal) -> {
+				values.put(name, (Double) newVal);
+				valueField.setText(String.format("%.2f", newVal));
+			});
 		}
 
-		protected String getName(){
+		protected String getName() {
 			return this.name;
 		}
-		
-		
+
 	}
 }
